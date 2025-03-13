e8645ddfd5503fb0a7568ee9441ad4db71918eeb
Taras Tkachk <t.t.fors37@gmail.com>
2025-03-13 19:53:46 +0200

    first

--- Praktika2.java
+++ Praktika2.java
@@ -50,6 +50,29 @@ class TextDisplay implements Displayable {
     }
 }
 
+class TableDisplay extends TextDisplay {
+    private int columnWidth;
+
+    public TableDisplay(int columnWidth) {
+        this.columnWidth = columnWidth;
+    }
+
+    @Override
+    public void display(List<ComputationData> computations) {
+        String format = "| %-" + columnWidth + "s | %-" + columnWidth + "s |\n";
+        System.out.printf(format, "Вхідне", "Результат");
+        System.out.println("-".repeat(columnWidth * 2 + 5));
+        for (ComputationData data : computations) {
+            System.out.printf(format, data.getInput(), data.getResult());
+        }
+    }
+
+    public void display(List<ComputationData> computations, String title) {
+        System.out.println("\n" + title);
+        display(computations);
+    }
+}
+
 abstract class DisplayFactory {
     public abstract Displayable createDisplay();
 }
@@ -58,6 +81,19 @@ class TextDisplayFactory extends DisplayFactory {
     @Override
     public Displayable createDisplay() {
         return new TextDisplay();
+    }
+}
+
+class TableDisplayFactory extends DisplayFactory {
+    private int columnWidth;
+
+    public TableDisplayFactory(int columnWidth) {
+        this.columnWidth = columnWidth;
+    }
+
+    @Override
+    public Displayable createDisplay() {
+        return new TableDisplay(columnWidth);
     }
 }
 
@@ -83,15 +119,34 @@ class Demo {
 
 public class ComputationTest {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         Solver solver = new Solver();
-        solver.compute(25);
-        solver.compute(16);
-        solver.compute(9);
 
-        DisplayFactory factory = new TextDisplayFactory();
+        System.out.println("Введіть кількість обчислень:");
+        int n = scanner.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("Введіть число для обчислення:");
+            double value = scanner.nextDouble();
+            solver.compute(value);
+        }
+
+        System.out.println("Виберіть спосіб виводу результатів:");
+        System.out.println("1 - Текстовий");
+        System.out.println("2 - Табличний");
+        int choice = scanner.nextInt();
+
+        DisplayFactory factory;
+        if (choice == 2) {
+            System.out.println("Введіть ширину колонки таблиці:");
+            int width = scanner.nextInt();
+            factory = new TableDisplayFactory(width);
+        } else {
+            factory = new TextDisplayFactory();
+        }
+
         Displayable display = factory.createDisplay();
-        
-        System.out.println("Результати обчислень:");
+
         display.display(solver.getComputations());
 
         String filename = "computations.ser";
